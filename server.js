9bf3db084f836c304893a1dac9ac461b7b079c1c
LeonardoGomez3006 <leonardo.gomez23b@udgvirtual.udg.mx>
2025-11-03 00:33:26 -0600

    Update

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname));
-app.get("*", (_req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-const port = process.env.PORT || 8003;
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 8003; 
 app.listen(port, () => console.log("listening on " + port));
